6ad419adc8736beb1fd92fab4a25d303dd677833
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2024-02-13 11:43:41 -0300

    Implement authentication feature

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  NotFoundException,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { UserService } from 'src/user/user.service';
+import { LoginDto } from './dtos/login.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(
+    private authService: AuthService,
+    private userService: UserService,
+  ) {}
+
+  @UsePipes(ValidationPipe)
+  @Post()
+  public async login(
+    @Body() loginData: LoginDto,
+  ): Promise<{ accessToken: string }> {
+    const findUserByEmail = await this.userService.findByEmail(
+      loginData?.email,
+    );
+
+    if (!findUserByEmail) throw new NotFoundException('User not found');
+
+    return await this.authService.login(loginData, findUserByEmail);
+  }
+}
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { UserModule } from 'src/user/user.module';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
 
 @Module({
-  imports: [],
-  providers: [],
-  controllers: [],
-  exports: [],
+  imports: [
+    UserModule,
+    JwtModule.registerAsync({
+      useFactory: () => ({
+        secret: process.env.JWT_SECRET,
+        signOptions: { expiresIn: process.env.JWT_EXPIRES_IN },
+      }),
+    }),
+  ],
+  providers: [AuthService],
+  controllers: [AuthController],
+  exports: [AuthService],
 })
 export class AuthModule {}
