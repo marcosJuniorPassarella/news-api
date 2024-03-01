b62b2b0a5423fdb62ba615911b6c729c94eb3c41
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2024-03-01 19:04:19 -0300

    Fix error

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,9 @@ import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { CategoriesModule } from './categories/categories.module';
 import { NewsModule } from './news/news.module';
-import { TestService } from './test/test.service';
 @Module({
   imports: [UserModule, AuthModule, CategoriesModule, NewsModule],
   controllers: [],
-  providers: [TestService],
+  providers: [],
 })
 export class AppModule {}
