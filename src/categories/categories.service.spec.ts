c8d51f40ff5da09dc23f89c7c272bc41fe395bd5
Marcos Júnior Passarella Naves <mjworks2020@outlook.com>
2024-03-09 14:55:39 -0300

    Implement unitary test of the CategoriesService

--- src/categories/categories.service.spec.ts
+++ src/categories/categories.service.spec.ts
@@ -1,6 +1,6 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { PrismaService } from '../prisma/prisma.service';
 import { CategoriesService } from './categories.service';
+import { Test, TestingModule } from '@nestjs/testing';
 import { categoriesMock, prismaCategoriesMock } from './mocks/categories.mock';
 
 describe(`${CategoriesService.name}`, () => {
@@ -23,7 +23,7 @@ describe(`${CategoriesService.name}`, () => {
     jest.clearAllMocks();
   });
 
-  it('should be defined', () => {
+  it(`should be defined`, () => {
     expect(service).toBeDefined();
   });
 
@@ -39,21 +39,18 @@ describe(`${CategoriesService.name}`, () => {
 
     expect(response).toEqual(categoriesMock[0]);
     expect(prismaService.categories.findFirst).toHaveBeenCalledTimes(1);
-    expect(prismaService.categories.findFirst).toHaveBeenCalledWith({
-      where: { name: categoriesMock[0].name },
-    });
   });
 
   it(`${CategoriesService.prototype.findByName.name}() should return null when category is not found`, async () => {
-    jest
-      .spyOn(prismaService.categories, 'findFirst')
-      .mockResolvedValueOnce(null);
-    const response = await service.findByName('fakeName');
+    jest.spyOn(prismaService.categories, 'findFirst').mockReturnValueOnce(null);
+    const response = await service.findByName('TESTE');
 
-    expect(response).toEqual(null);
+    expect(response).toBeNull();
     expect(prismaService.categories.findFirst).toHaveBeenCalledTimes(1);
     expect(prismaService.categories.findFirst).toHaveBeenCalledWith({
-      where: { name: 'fakeName' },
+      where: {
+        name: 'TESTE',
+      },
     });
   });
 
@@ -69,21 +66,18 @@ describe(`${CategoriesService.name}`, () => {
 
     expect(response).toEqual(categoriesMock[0]);
     expect(prismaService.categories.findFirst).toHaveBeenCalledTimes(1);
-    expect(prismaService.categories.findFirst).toHaveBeenCalledWith({
-      where: { id: categoriesMock[0].id },
-    });
   });
 
   it(`${CategoriesService.prototype.findById.name}() should return null when category is not found`, async () => {
-    jest
-      .spyOn(prismaService.categories, 'findFirst')
-      .mockResolvedValueOnce(null);
-    const response = await service.findById('123');
+    jest.spyOn(prismaService.categories, 'findFirst').mockReturnValueOnce(null);
+    const response = await service.findById('123456');
 
-    expect(response).toEqual(null);
+    expect(response).toBeNull();
     expect(prismaService.categories.findFirst).toHaveBeenCalledTimes(1);
     expect(prismaService.categories.findFirst).toHaveBeenCalledWith({
-      where: { id: '123' },
+      where: {
+        id: '123456',
+      },
     });
   });
 
@@ -98,35 +92,35 @@ describe(`${CategoriesService.name}`, () => {
 
     expect(response.name).toEqual(categoriesMock[1].name);
     expect(prismaService.categories.update).toHaveBeenCalledTimes(1);
-    expect(prismaService.categories.update).toHaveBeenCalledWith({
-      where: { id: categoriesMock[0].id },
-      data: categoriesMock[1],
-    });
   });
 
   it(`${CategoriesService.prototype.update.name}() should return null when category is not found`, async () => {
-    jest.spyOn(prismaService.categories, 'update').mockResolvedValueOnce(null);
+    jest.spyOn(prismaService.categories, 'update').mockReturnValueOnce(null);
     const response = await service.update({
-      id: '123',
+      id: `123`,
       data: categoriesMock[0],
     });
 
-    expect(response).toEqual(null);
+    expect(response).toBeNull();
     expect(prismaService.categories.update).toHaveBeenCalledTimes(1);
     expect(prismaService.categories.update).toHaveBeenCalledWith({
-      where: { id: '123' },
+      where: {
+        id: '123',
+      },
       data: categoriesMock[0],
     });
   });
 
-  it(`${CategoriesService.prototype.update.name}() should delete category`, async () => {
-    jest.spyOn(prismaService.categories, 'delete').mockResolvedValueOnce(null);
+  it(`${CategoriesService.prototype.delete.name}() should delete category`, async () => {
+    jest.spyOn(prismaService.categories, 'delete').mockReturnValueOnce(null);
     const response = await service.delete(categoriesMock[0].id);
 
-    expect(response).toEqual(null);
+    expect(response).toBeNull();
     expect(prismaService.categories.delete).toHaveBeenCalledTimes(1);
     expect(prismaService.categories.delete).toHaveBeenCalledWith({
-      where: { id: categoriesMock[0].id },
+      where: {
+        id: categoriesMock[0].id,
+      },
     });
   });
 });
